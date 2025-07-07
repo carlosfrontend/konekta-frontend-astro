09b10e9712810ca4243ac9015b9e0c4470776c20
carlosfrontend <11649745+carlosfrontend@users.noreply.github.com>
2025-07-07 18:55:21 +0200

    feat: Add Konekta Frontend Conference components and schedule
    - Implemented Footer component with social media links.
    - Created DownArrowIcon, GitHubIcon, LinkedinIcon, MouseIcon, and XIcon components.
    - Developed RegistrationForm component with validation and success modal.
    - Added ScheduleSection component to display event schedule dynamically.
    - Introduced SpeakersSection component to showcase speakers with images.
    - Populated schedule data with sessions and speakers for the conference.
    - Added speakers data with images and details for each speaker.
    - Included new speaker image for Patricia Lebsack.

---
+++ src/data/speakers.ts
@@ -0,0 +1,50 @@
+import jeanGrahamImg from '../assets/images/speakers/jean_graham.jpg';
+import ervinHowellImg from '../assets/images/speakers/ervin_howell.jpg';
+import clementineBauchImg from '../assets/images/speakers/clementine_bauch.jpg';
+import patriciaLebsackImg from '../assets/images/speakers/patricia_lebsack.jpg';
+import chelseyDietrichImg from '../assets/images/speakers/chelsey_dietrich.jpg';
+import dennisSchulistImg from '../assets/images/speakers/dennis_schulist.jpg';
+
+const speakersData = [
+  {
+    "id": 1,
+    "name": "Jean Graham",
+    "position": "CEO & Fundador, Tech Innovators",
+    "image": jeanGrahamImg
+  },
+  {
+    "id": 2,
+    "name": "Ervin Howell",
+    "position": "Arquitecto de Soluciones Cloud, Global Corp",
+    "image": ervinHowellImg
+  },
+  {
+    "id": 3,
+    "name": "Clementine Bauch",
+    "position": "Directora de Producto, Startup X",
+    "image": clementineBauchImg
+  },
+  {
+    "id": 4,
+    "name": "Patricia Lebsack",
+    "position": "Ingeniera de Machine Learning, AI Driven",
+    "image": patriciaLebsackImg
+  },
+  {
+    "id": 5,
+    "name": "Chelsey Dietrich",
+    "position": "Desarrolladora Frontend Senior, Pixel Perfect",
+    "image": chelseyDietrichImg
+  },
+  {
+    "id": 6,
+    "name": "Mrs. Dennis Schulist",
+    "position": "Experto en Ciberseguridad, Secure Systems",
+    "image": dennisSchulistImg
+  }
+]
+  ;
+
+
+
+export default speakersData
