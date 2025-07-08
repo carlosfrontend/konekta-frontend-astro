e637106f5858b3e1407b6afd49c7f647bf94d9ae
carlosfrontend <11649745+carlosfrontend@users.noreply.github.com>
2025-07-08 20:13:58 +0200

    feat: Add Google Fonts integration and remove local Manrope font files

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,5 @@
-// @ts-check
-import { defineConfig } from 'astro/config';
+// @ts-nocheck
+import { defineConfig, fontProviders } from 'astro/config';
 
 import tailwindcss from '@tailwindcss/vite';
 
@@ -7,6 +7,26 @@ import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
+  experimental: {
+      fonts: [
+      {
+        name: "Mochiy Pop P One",
+        cssVariable: "--font-headings",
+        provider: fontProviders.google(),
+        subsets: ["latin"],
+        weights: [400],
+        styles: ["normal"],
+      },
+      {
+        name: "Manrope",
+        cssVariable: "--font-primary",
+        provider: fontProviders.google(),
+        subsets: ["latin"],
+        weights: [200, 300, 400, 500, 600, 700, 800],
+      }
+
+    ]
+  },
   vite: {
     plugins: [tailwindcss()]
   },
