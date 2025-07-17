1badfbbd148c7970352919b84cfd8dae88975bf6
carlosfrontend <11649745+carlosfrontend@users.noreply.github.com>
2025-07-17 12:23:28 +0200

    feat: Add active classes to menu links when scrolling to each section

--- src/data/links.ts
+++ src/data/links.ts
@@ -1,10 +1,10 @@
 import type { Link } from '../types'
 
-const GENERIC_CLASS = 'text-lg text-slate-700 transition-transform ease-out hover:scale-110 hover:text-sky-700 inline-block'
+const GENERIC_CLASS = 'text-lg text-slate-700 transition-transform ease-out hover:scale-110 hover:text-sky-700 inline-block focus:text-sky-700'
 
 const CALL_TO_ACTION_CLASS_DESKTOP = 'flex text-lg font-bold text-white bg-pink-700 px-4 py-2 transition-transform ease-in-out duration-300 hover:scale-95 hover:bg-pink-800 hover:outline-2 hover:outline-white rounded-md'
 
-const GENERIC_CLASS_MOBILE = 'text-2xl text-slate-700'
+const GENERIC_CLASS_MOBILE = 'text-2xl text-slate-700 focus:text-sky-700 inline-block'
 
 const CALL_TO_ACTION_CLASS_MOBILE = 'flex text-2xl font-semibold text-white bg-pink-700 px-4 py-2 rounded-md'
 
